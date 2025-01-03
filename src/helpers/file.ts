5235b8ea4201b3097cdc3787d2d6ad5cc4c6b59e
idsl <idsl5397@gmail.com>
2025-01-03 15:45:20 +0800

    更正

--- src/helpers/file.ts
+++
@@ -1,9 +0,0 @@
-import type { ProcessServerConfigFunction } from "filepond";
-
-const dummyFileProcessHandler: ProcessServerConfigFunction = (_, __, ___, load) => {
-    load({ message: "done" });
-};
-
-export const fileHelper = {
-    dummyFileProcessHandler,
-};
--- src/store/useGlobalStore.tsx
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';  // 需要引入 persist middleware
-
-interface user{
-    email: string;
-    username: string;
-}
-
