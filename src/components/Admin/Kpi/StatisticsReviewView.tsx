bd7f3961e0551a31eba2bd319d1f988c627c089a
webapi <idsl5397@gmail.com>
2026-02-24 12:04:32 +0800

    修改 ESLint 規則

--- src/components/Admin/Kpi/StatisticsReviewView.tsx
+++ src/components/Admin/Kpi/StatisticsReviewView.tsx
@@ -130,7 +130,7 @@ export default function StatisticsReviewView() {
     const toggleRow = (id: number) => {
         setSelectedIds((prev) => {
             const next = new Set(prev);
-            next.has(id) ? next.delete(id) : next.add(id);
+            if (next.has(id)) { next.delete(id); } else { next.add(id); }
             return next;
         });
     };
